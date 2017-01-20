f99b49f4d8c9c20d3d6b0ea95f0f1be9411a7bd3
nsaiisasidhar <sasinimmagadda@gmail.com>
2017-01-20 17:03:47 -0500

    Create InstructorEditStudentFeedbackPage.java

---
+++ src/test/java/teammates/test/pageobjects/InstructorEditStudentFeedbackPage.java
@@ -0,0 +1,28 @@
+package teammates.test.pageobjects;  
+
+  import java.io.IOException;
+
+  import org.openqa.selenium.By;
+  import org.openqa.selenium.JavascriptExecutor;		  
+  import org.openqa.selenium.WebElement;	
+  
+  public class InstructorEditStudentFeedbackPage extends FeedbackSubmitPage {
+   
+     public void clickModerationHintButton() {
+         click(By.id("moderationHintButton"));
+         }
+    
+     public boolean isModerationHintVisible() {
+         return isElementVisible("moderationHint");
+         }
+    
+     public void verifyModerationHeaderHtml(String filePathParam) throws IOException {
+         verifyHtmlPart(By.className("navbar"), filePathParam);
+         }
+    
+     public String getModerationHintButtonText() {
+         WebElement moderationHintButton = browser.driver.findElement(By.id("moderationHintButton"));
+         return moderationHintButton.getText();
+         }
+         
+   }
