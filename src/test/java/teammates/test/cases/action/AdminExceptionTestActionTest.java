de737379f85bac5919909872dbeb7c0425553e06
nsaiisasidhar <sasinimmagadda@gmail.com>
2017-04-13 17:12:59 -0400

    Update AdminExceptionTestActionTest.java

--- src/test/java/teammates/test/cases/action/AdminExceptionTestActionTest.java
+++ src/test/java/teammates/test/cases/action/AdminExceptionTestActionTest.java
@@ -4,14 +4,24 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import teammates.common.util.Const;
+import teammates.common.util.Sanitizer;
+import teammates.common.util.AppUrl;
+import teammates.ui.controller.AdminExceptionTestAction;
+import teammates.test.pageobjects.AppPage;
+import teammates.test.pageobjects.Browser;
+import teammates.test.pageobjects.BrowserPool;
 
 public class AdminExceptionTestActionTest extends BaseActionTest {
 
     // private final DataBundle dataBundle = getTypicalDataBundle();
+    private static Browser browser;
+    private static AppPage page;
     
     @BeforeClass
     public void classSetup() {
         printTestClassHeader();
+        browser = BrowserPool.getBrowser();
+        page = loginAdmin(browser);
         uri = Const.ActionURIs.ADMIN_EXCEPTION_TEST;
         // removeAndRestoreTypicalDataInDatastore();
     }
@@ -19,7 +29,8 @@ public class AdminExceptionTestActionTest extends BaseActionTest {
     @Test
     public void testExecuteAndPostProcess() {
         
-        //TODO: implement this
+        AppUrl url = new AppUrl().createUrl(uri).withParam(Const.ParamsNames.ERROR, AssertionError.class.getSimpleName());
+        page.navigateTo(url);     
     }
 
 }
